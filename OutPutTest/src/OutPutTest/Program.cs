dbdb8fc7b22bab6f7229efd52678d1237b26cd2f
赵一鸣 <245292110@qq.com>
2016-11-30 11:06:26 +0800

    checkin1

---
+++ OutPutTest/src/OutPutTest/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Threading.Tasks;
+
+namespace OutPutTest
+{
+    public class Program
+    {
+        public void Main(string[] args)
+        {
+            Console.WriteLine("1");
+            Console.WriteLine("2");
+
+            Console.Read();
+        }
+    }
+}
